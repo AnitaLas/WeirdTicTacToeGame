1323d3adea7f937666c32b19ecd9a5a4c9f70833
﻿Liliana Niedzwiedz <liliananiedzwiedz@gmail.com>
2024-03-28 09:45:31 +0100

    20230328
    20230328
    > added verification for buttons randomly and for all

---
+++ Assets/Scripts/GameConfiguration/GameConfigurationButtonsWithNumbers/GameConfigurationButtonsWithNumbersForChangeRandomlyAndForAll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class GameConfigurationButtonsWithNumbersForChangeRandomlyAndForAll
+    {
+
+        public static void SetUpDefaultTime(string gameObjectTagName)
+        {
+            GameObject button = CommonMethods.GetObjectByTagName(gameObjectTagName);
+            string defaultTime = GameConfigurationButtonsCommonButtonsDefaultNumber.GetDefaultButtonTimeForChange();
+            CommonMethods.ChangeTextForCubePlay(button, defaultTime);
+        }
+
+        public static int SetUpChosenTimeForConfigurationRandomly(GameObject[,,] _buttonsWithNumbers, string gameObjectName)
+        {
+            string tagConfigurationBoardGameChangeNumberForAll = GameConfigurationButtonsCommonButtonsTagName.GetTagForButtonNumberByTagChangeNumberForAll();
+            SetUpDefaultTime(tagConfigurationBoardGameChangeNumberForAll);
+            int time = GameConfigurationButtonsCommonMethods.SetUpChosenNumberForConfigurationRandomly(_buttonsWithNumbers, gameObjectName);
+            return time;
+        }
+
+        public static int SetUpChosenTimeForConfigurationForAll(GameObject[,,] _buttonsWithNumbers, string gameObjectName)
+        {
+            string tagConfigurationBoardGameChangeNumberRandomly = GameConfigurationButtonsCommonButtonsTagName.GetTagForButtonNumberByTagChangeNumberRandomly();
+            SetUpDefaultTime(tagConfigurationBoardGameChangeNumberRandomly);
+            int time = GameConfigurationButtonsCommonMethods.SetUpChosenNumberForConfigurationForAll(_buttonsWithNumbers, gameObjectName);
+            return time;
+        }
+
+    }
+}
